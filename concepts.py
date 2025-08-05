7b981a144bb97c93615017cdc2041e2fbbf1ccab
tartar.sawce <amnairfantarar@gmail.com>
2025-08-05 18:12:26 +0500

    wwfg

---
+++ concepts.py
@@ -0,0 +1,40 @@
+class Coffee:
+    # pass
+
+
+#     def __init__(self):
+#         # print("Coffee is created")
+#         self.temperature = 0
+#         self.flavour = 'sweet, smoky, sumatran'
+    
+
+# c = Coffee()
+# # print(c)
+
+
+# myCoffee = Coffee()
+# print(myCoffee.flavour)
+# print(c.temperature)
+
+    def __init__(self, temp = 0, flavour = 'bland', origin = 'yemen'):
+        # above attributes default?
+        self.temperature = temp
+        self.flavour = flavour
+        self.origin = origin
+    
+    def change_temp(self, temp):
+        self.temperature = temp
+
+ethans_coffee = Coffee(80, 'spicy')
+# print(ethans_coffee.flavour)
+
+brandons_coffee = Coffee(90)
+# print(brandons_coffee.temperature)
+# print(brandons_coffee.flavour)
+print(brandons_coffee.origin)
+
+brandons_coffee.change_temp(120)
+# print(brandons_coffee.temperature)
+
+amnas_coffee = Coffee(100, 'fruity', 'ethiopia')
+# print(amnas_coffee.origin)
